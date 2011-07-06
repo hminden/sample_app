0080592bb5bbc1f25adaab640c0647a11427adaf
tron <tron@data.caitin.inc>
2011-07-06 16:46:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '68f979ea5a836daadbc60755b8713fdf74c39902fa6f1e622c2f8983c31165ee8aef1411f0be159105dab8d2090980e87eae970833748590b2b136c79839a0ac'
